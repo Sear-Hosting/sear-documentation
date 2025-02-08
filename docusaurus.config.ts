ebfe64877cee98dc50165c5f15227a3437baf2f9
Ashank <ashank@ashank.tech>
2025-02-08 13:09:46 +0800

    Add initial Docusaurus setup with example pages and styles

---
+++ docusaurus.config.ts
@@ -0,0 +1,118 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'Sear Hosting Documentation',
+  tagline: 'Welcome to Sear Hosting\'s Documentation',
+  favicon: 'img/favicon.ico',
+
+  url: 'https://docs.searhosting.com',
+  baseUrl: '/',
+
+  organizationName: 'sear-hosting',
+  projectName: 'documentation',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          routeBasePath: '/', // Docs will be served at root
+          breadcrumbs: true,
+        },
+        blog: false, // Disable blog
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    colorMode: {
+      defaultMode: 'dark',
+      respectPrefersColorScheme: false,
+    },
+    navbar: {
+      title: 'Sear Hosting',
+      logo: {
+        alt: 'Sear Hosting Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Documentation',
+        },
+        {
+          href: 'https://github.com/sear-hosting',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Documentation',
+          items: [
+            {
+              label: 'Getting Started',
+              to: '/docs/getting-started',
+            },
+            {
+              label: 'API Reference',
+              to: '/docs/api',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/sear-hosting',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discord.gg/searhosting',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Status',
+              href: 'https://status.searhosting.com',
+            },
+            {
+              label: 'Contact',
+              href: 'https://searhosting.com/contact',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Sear Hosting. All rights reserved.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
